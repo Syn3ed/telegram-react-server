58f55e1341959d0d07bb1630b6cbd99d8772cf42
Syn3ed <lol-uuu@bk.ru>
2023-11-27 14:26:29 +0700

    modified:   src/BaseData/bdConnect.js

--- src/BaseData/bdConnect.js
+++ src/BaseData/bdConnect.js
@@ -11,5 +11,5 @@ require('dotenv').config();
 //   });
 const sequelize = new Sequelize('postgres://syne3d_user:YwvXuLjxMvufBB5w1h0jPsjLA4ztJqR0@dpg-clg7277jc5ks73ecg5n0-a/syne3d');
 
-
+//
   module.exports = sequelize
