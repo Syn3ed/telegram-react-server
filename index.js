6999e4d07e89ed6e0c8cf0a6b9e0d4467eb67571
Syn3ed <lol-uuu@bk.ru>
2024-01-16 11:39:19 +0700

    modified:   index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const multer = require('multer');
 const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TELEGRAM_BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
