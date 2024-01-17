8d82700d395ce0cae5073683548889f9aa8b1092
Syn3ed <lol-uuu@bk.ru>
2024-01-17 13:24:28 +0700

    modified:   src/BaseData/bdConnect.js

--- src/BaseData/bdConnect.js
+++ src/BaseData/bdConnect.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 //     password: 'YwvXuLjxMvufBB5w1h0jPsjLA4ztJqR0', 
 //     database: 'syne3d', 
 //   });
-const sequelize = new Sequelize('postgres://syne3d_user:YwvXuLjxMvufBB5w1h0jPsjLA4ztJqR0@dpg-clg7277jc5ks73ecg5n0-a/syne3d');
+const sequelize = new Sequelize('postgres://syne3d_user:YwvXuLjxMvufBB5w1h0jPsjLA4ztJqR0@dpg-clg7277jc5ks73ecg5n0-a.frankfurt-postgres.render.com/syne3d');
 
 //
   module.exports = sequelize
