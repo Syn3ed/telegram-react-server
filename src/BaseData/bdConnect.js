c0c2c965e1dd7a03b2104bf4f2b46f694d066194
Syn3ed <lol-uuu@bk.ru>
2024-02-26 11:47:46 +0700

    modified:   src/BaseData/bdConnect.js

--- src/BaseData/bdConnect.js
+++ src/BaseData/bdConnect.js
@@ -1,15 +1,16 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config(); 
 
-// const sequelize = new Sequelize({
-//     dialect: 'postgres', 
-//     host: 'dpg-clg7277jc5ks73ecg5n0-a',   
-//     port: 5432,          
-//     username: 'syne3d_user', 
-//     password: 'YwvXuLjxMvufBB5w1h0jPsjLA4ztJqR0', 
-//     database: 'syne3d', 
-//   });
-const sequelize = new Sequelize('postgres://syne3d_user:YwvXuLjxMvufBB5w1h0jPsjLA4ztJqR0@dpg-clg7277jc5ks73ecg5n0-a.frankfurt-postgres.render.com/syne3d?ssl=true');
+const sequelize = new Sequelize({
+    dialect: 'postgres', 
+    host: 'localhost',   
+    port: 5432,          
+    username: 'postgres', 
+    password: '123456789', 
+    database: 'TgBotBD', 
+  });
+
+// const sequelize = new Sequelize('postgres://syne3d_user:YwvXuLjxMvufBB5w1h0jPsjLA4ztJqR0@dpg-clg7277jc5ks73ecg5n0-a.frankfurt-postgres.render.com/syne3d?ssl=true');
 
 
 //
