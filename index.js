c035522df32f8e366a2c6c6ee276bcd4cc018ae9
Syn3ed <lol-uuu@bk.ru>
2024-04-22 09:32:46 +0700

    modified:   index.js

--- index.js
+++ index.js
@@ -2379,7 +2379,9 @@ const startBot = async () => {
           });
           if (userId === messages[0].UserRequest.User.telegramId) {
             bot.sendMessage(userId, `Вы закрыли заявку №${requestId}`);
-            await bot.sendMessage(messages[0].operatorId, `Пользователь закрыл заявку №${requestId}`);
+            if (messages[0]?.operatorId) {
+              await bot.sendMessage(messages[0]?.operatorId, `Пользователь закрыл заявку №${requestId}`);
+            }
           } else {
             bot.sendMessage(userId, `Вы закрыли заявку №${requestId} `);
             bot.sendMessage(messages[0].UserRequest.User.telegramId, `Оператор закрыл вашу заявку №${requestId}`)
